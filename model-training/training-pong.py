28135cf8b1b0f6323d0e6cae447159639ed18b2a
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2024-06-18 13:43:39 +0300

    bouncing sorted

--- model-training/training-pong.py
+++ model-training/training-pong.py
@@ -1,18 +1,48 @@
 import pygame
+import random
 
 ASPECT = 100
 WIDTH, HEIGHT = 8 * ASPECT, 6 * ASPECT
 BALL_R = 0.1 * ASPECT
 PADDLE_WIDTH = 0.1 * ASPECT
-PADDLE_HEIGHT = 0.3 * ASPECT
+PADDLE_HEIGHT = 3 * ASPECT
 VEL = 0.1 * ASPECT
+BALL_START_VEL = 0.04 * ASPECT
 
 FPS = 60
 
 class Ball():
     def __init__(self):
-        self.x = WIDTH // 2
-        self.y = HEIGHT // 3
+        self.x = WIDTH
+        self.y = 0
+        self.x_vel = -BALL_START_VEL
+        self.y_vel = 3
+        self.dead = False
+    
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+        if self.y < BALL_R:
+            self.y = self.y * -1 + BALL_R
+            self.y_vel = self.y_vel * -1
+        
+        elif self.y > HEIGHT - BALL_R:
+            self.y = 2 * HEIGHT - self.y - BALL_R
+            self.y_vel = self.y_vel * -1
+
+        if self.x > WIDTH - BALL_R:
+            self.x = 2 * WIDTH - self.x - BALL_R
+            self.x_vel = self.x_vel * -1
+            self.y_vel = random.randint(0, 10)
+        
+        elif self.x < BALL_R:
+            self.dead = True
+
+    def bounce(self, y_dif):
+        self.y_vel = self.y_vel + y_dif
+        self.x_vel = self.x_vel * -1
+
 
 class Paddle():
     def __init__(self):
@@ -30,6 +60,12 @@ class Paddle():
         if self.y > HEIGHT - PADDLE_HEIGHT:
             self.y = HEIGHT - PADDLE_HEIGHT
 
+def handle_collision(ball: Ball, paddle: Paddle):
+    if ball.x_vel < 0 and not ball.dead and ball.x <= paddle.x + PADDLE_WIDTH and ball.y >= paddle.y and ball.y <= paddle.y + PADDLE_HEIGHT:
+        y_dif = paddle.y + PADDLE_HEIGHT // 2 - ball.y
+        y_dif = HEIGHT // 2 // -y_dif 
+        ball.bounce(y_dif)
+        
 
 
 pygame.init()
@@ -41,6 +77,7 @@ def gameLoop():
 
     running = True
     paddle = Paddle()
+    ball = Ball()
 
     while running:
 
@@ -60,7 +97,10 @@ def gameLoop():
 
         # Game
 
+        ball.move()
+        handle_collision(ball, paddle)
 
+        pygame.draw.circle(win, "white", (ball.x, ball.y), BALL_R)
         pygame.draw.rect(win, "white", (paddle.x, paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT))
 
 
