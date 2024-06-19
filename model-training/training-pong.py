a0ee65572205ad003a4018ee2d40536acc5a76e5
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2024-06-19 15:36:26 +0300

    finish model training, extract constants

--- model-training/training-pong.py
+++ model-training/training-pong.py
@@ -1,3 +1,4 @@
+import json
 import pygame
 import random
 import os
@@ -16,17 +17,35 @@ Y_DIFF_FACTOR = 15
 BALL_START_VEL = 0.04 * ASPECT
 MAX_BALLS_NUMBER = 3
 
+with open("gameConfig.json", "w") as f:
+    json.dump(
+        {
+            "aspect": ASPECT,
+            "width": WIDTH // ASPECT,
+            "height": HEIGHT // ASPECT,
+            "ballR": BALL_R / ASPECT,
+            "paddleWidth": PADDLE_WIDTH / ASPECT,
+            "paddleHeight": PADDLE_HEIGHT / ASPECT,
+            "velocity": VEL / ASPECT,
+            "maxYVelocity": MAX_Y_VEL / ASPECT,
+            "yDiffFactor": Y_DIFF_FACTOR,
+            "ballStartVelocity": BALL_START_VEL / ASPECT,
+            "maxBalls": MAX_BALLS_NUMBER,
+        },
+        f,
+    )
+
 FPS = 300
 
 
-class Ball():
+class Ball:
     def __init__(self):
         self.x = WIDTH
         self.y = 0
         self.x_vel = -BALL_START_VEL
         self.y_vel = 3
         self.dead = False
-    
+
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
@@ -34,7 +53,7 @@ class Ball():
         if self.y < BALL_R:
             self.y = self.y * -1 + BALL_R
             self.y_vel *= -1
-        
+
         elif self.y > HEIGHT - BALL_R:
             self.y = 2 * HEIGHT - self.y - BALL_R
             self.y_vel *= -1
@@ -43,7 +62,7 @@ class Ball():
             self.x = 2 * WIDTH - self.x - BALL_R
             self.x_vel *= -1
             self.y_vel = random.randint(0, 10)
-        
+
         elif self.x < BALL_R:
             self.dead = True
 
@@ -56,7 +75,7 @@ class Ball():
         self.x_vel *= -1
 
 
-class Paddle():
+class Paddle:
     def __init__(self):
         self.x = 0 + BALL_R * 2
         self.y = 0
@@ -66,14 +85,21 @@ class Paddle():
             self.y -= VEL
         else:
             self.y += VEL
-        
+
         if self.y < 0:
             self.y = 0
         if self.y > HEIGHT - PADDLE_HEIGHT:
             self.y = HEIGHT - PADDLE_HEIGHT
 
+
 def handle_collision(ball: Ball, paddle: Paddle) -> bool:
-    if ball.x_vel < 0 and not ball.dead and ball.x <= paddle.x + PADDLE_WIDTH and ball.y >= paddle.y and ball.y <= paddle.y + PADDLE_HEIGHT:
+    if (
+        ball.x_vel < 0
+        and not ball.dead
+        and ball.x <= paddle.x + PADDLE_WIDTH
+        and ball.y >= paddle.y
+        and ball.y <= paddle.y + PADDLE_HEIGHT
+    ):
         y_dif = ball.y - (paddle.y + PADDLE_HEIGHT // 2)
         y_dif = (y_dif / PADDLE_HEIGHT) * Y_DIFF_FACTOR
         ball.bounce(round(y_dif))
@@ -81,8 +107,6 @@ def handle_collision(ball: Ball, paddle: Paddle) -> bool:
         return True
 
     return False
-        
-
 
 
 def gameLoop(genome, config):
@@ -108,7 +132,7 @@ def gameLoop(genome, config):
             if event.type == pygame.QUIT:
                 running = False
                 quit()
-        
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
             running = False
@@ -118,9 +142,18 @@ def gameLoop(genome, config):
         # if keys[pygame.K_DOWN] or keys[pygame.K_s]:
         #     paddle.move("down")
 
-        nearest_ball_index = balls.index(min(balls, key=lambda x: x.x if x.x_vel < 0 else WIDTH))
+        nearest_ball_index = balls.index(
+            min(balls, key=lambda x: x.x if x.x_vel < 0 else WIDTH)
+        )
 
-        output = net.activate((paddle.y + PADDLE_HEIGHT // 2, balls[nearest_ball_index].y, balls[nearest_ball_index].x, balls[nearest_ball_index].y_vel))
+        output = net.activate(
+            (
+                paddle.y + PADDLE_HEIGHT // 2,
+                balls[nearest_ball_index].y,
+                balls[nearest_ball_index].x,
+                balls[nearest_ball_index].y_vel,
+            )
+        )
 
         max_index = np.argmax(output)
 
@@ -136,8 +169,10 @@ def gameLoop(genome, config):
             ball.move()
             is_increment = handle_collision(ball, paddle)
             pygame.draw.circle(win, "white", (ball.x, ball.y), BALL_R)
-        
-        pygame.draw.rect(win, "white", (paddle.x, paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT))
+
+        pygame.draw.rect(
+            win, "white", (paddle.x, paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT)
+        )
 
         # Update screen
 
@@ -159,34 +194,45 @@ def gameLoop(genome, config):
             if ball.dead:
                 running = False
                 genome.fitness -= 1
-        
+
             if is_increment:
                 score += 1
                 genome.fitness += 1
                 is_increment = False
 
+        if genome.fitness >= 410:
+            running = False
 
     pygame.quit()
+
 
 # NEAT setup
 
 local_dir = os.path.dirname(__file__)
 config_path = os.path.join(local_dir, "config-ff.txt")
-config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+config = neat.config.Config(
+    neat.DefaultGenome,
+    neat.DefaultReproduction,
+    neat.DefaultSpeciesSet,
+    neat.DefaultStagnation,
+    config_path,
+)
 
 # population = neat.Checkpointer.restore_checkpoint('neat-checkpoint-1')
 population = neat.Population(config)
 population.add_reporter(neat.StdOutReporter(True))
 stats = neat.StatisticsReporter()
 population.add_reporter(stats)
-# population.add_reporter(neat.Checkpointer(10))
+population.add_reporter(neat.Checkpointer(10))
+
 
 def run_all_genomes(genomes, config):
 
-    for (_genome_id, genome) in genomes:
+    for _genome_id, genome in genomes:
         genome.fitness = 0
         gameLoop(genome, config)
 
 
 winner = population.run(run_all_genomes, 1500)
-pickle.dump(winner)
+with open("winner.pickle", "wb") as f:
+    pickle.dump(winner, f)
